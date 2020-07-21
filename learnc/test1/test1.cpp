519768e1b7641e1b76ab654f95606433eb7f1d3a
xinfenji <xinfenji@sohu.com>
2020-07-21 16:43:32 +0800

    0721 add a file for learnjs

---
+++ learnc/test1/test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+   //     using namespace std;
+        cout<<"helo"<<endl;
+        cout<<"sizeof int is "<<sizeof(int)<<" bytes"<<endl;
+        return 0;
+        
+        
+}
