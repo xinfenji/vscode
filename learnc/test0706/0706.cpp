f3db734e78743c7752c8234e2e2dee583b1ab658
xinfenji <xinfenji@sohu.com>
2020-07-28 16:12:31 +0800

    0728

--- learnc/test0706/0706.cpp
+++ learnc/test0706/0706.cpp
@@ -2,12 +2,14 @@
 #include <fstream>
 using namespace std;
 
-int main() {
+int main() 
+{
         char data[100];
         ofstream outfile;
         outfile.open("afile.dat");
 
-        cout<<"Write to the file"<<endl;
+        cout << "Write to the file" << endl;
+   //     cout<<endl;
         cout<<"Enter your name:";
         cin.getline(data,100);
 
