fc9ee21e2bc31694cbe55d33be1ffd5756e852ae
xinfenji <xinfenji@sohu.com>
2020-07-29 16:36:16 +0800

    0728.cpp change 07291630

--- learnc/test0728/t728.cpp
+++ learnc/test0728/t728.cpp
@@ -1,32 +1,39 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-        const char key[]={'a','c','b','a','d'};
-        const int NUM_QUES=5;
+int main()
+{
+        const char key[] = {'a', 'c', 'b', 'a', 'd'};
+        const int NUM_QUES = 5;
         char c;
-        int ques=0,numCorrect=0;
-        cout<<"Enter the"<<NUM_QUES<<"questions test number end with the 'ENTER'"<<endl;
-        while(cin.get(c)){
-                if(c!= '\n'){
-                        if (c=key[ques]){
+        int ques = 0; 
+        int numCorrect = 0;
+        cout << "Enter the" << NUM_QUES << "questions test number end with the 'ENTER'" << endl;
+        while (cin.get(c))
+        {
+                if (c != '\n')
+                {
+                        if (c = key[ques])
+                        {
                                 numCorrect++;
-                                cout<<"@";
-
+                                cout << " ";
+                       //         ques++;
                         }
                         else
-                        {
-                                cout<<"*";
-                        }
-                       ques++;
-
-                }else{
-                        ques=0,numCorrect=0;
-                        cout<<endl;
-                        cout<<"The rate of the correct anser is"<<numCorrect/NUM_QUES<<endl;
+                        
+                                cout << "*";
+                       //         ques++;
+                        
+                        ques++;
+                }
+                else
+                {
+                       // ques = 0, numCorrect = 0;
+                       // cout << endl;
+                        cout << "The rate of the correct anser is" << static_cast<float>(numCorrect) / NUM_QUES *100<<"%"<< endl;
+                        ques=0;numCorrect=0;cout<<endl;
                 }
         }
         return 0;
-
 }
