989dd340bfef64be3c1ec05ffcd46e308f64e92f
xinfenji <xinfenji@sohu.com>
2020-07-29 16:42:50 +0800

    change

--- learnc/test0728/t728.cpp
+++ learnc/test0728/t728.cpp
@@ -7,7 +7,7 @@ int main()
         const char key[] = {'a', 'c', 'b', 'a', 'd'};
         const int NUM_QUES = 5;
         char c;
-        int ques = 0; 
+        int ques = 0;
         int numCorrect = 0;
         cout << "Enter the" << NUM_QUES << "questions test number end with the 'ENTER'" << endl;
         while (cin.get(c))
@@ -18,21 +18,24 @@ int main()
                         {
                                 numCorrect++;
                                 cout << " ";
-                       //         ques++;
+                                //         ques++;
                         }
                         else
-                        
+                        {
+
                                 cout << "*";
-                       //         ques++;
-                        
+                                //         ques++;
+                        }
                         ques++;
                 }
                 else
                 {
-                       // ques = 0, numCorrect = 0;
-                       // cout << endl;
-                        cout << "The rate of the correct anser is" << static_cast<float>(numCorrect) / NUM_QUES *100<<"%"<< endl;
-                        ques=0;numCorrect=0;cout<<endl;
+                        // ques = 0, numCorrect = 0;
+                        // cout << endl;
+                        cout << "The rate of the correct anser is" << static_cast<float>(numCorrect) / NUM_QUES * 100 << "%" << endl;
+                        ques = 0;
+                        numCorrect = 0;
+                        cout << endl;
                 }
         }
         return 0;
